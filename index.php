8535bee491d60911ffb8cfe0e2d4fce3f2e69621
osama <osama@vbox.myguest.virtualbox.org>
2024-12-08 22:15:25 +0100

    changed some stuff

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
-// Include the database configuration
 
 
-echo "bochta is gay <br>";
+echo "ok this works <br>";
 
 
 include 'db-config.php';
 
-// Query the database
 $sql = "SELECT FirstName FROM clients";
 $result = $conn->query($sql);
 
-// Check if there are results
 if ($result->num_rows > 0) {
     echo "<table border='1'>";
     echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
 
-    // Output data for each row
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["FirstName"] . "</td>";
@@ -28,6 +24,5 @@ if ($result->num_rows > 0) {
     echo "No results found.";
 }
 
-// Close the connection
 $conn->close();
 ?>
